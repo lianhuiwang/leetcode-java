47f5c7c822fa93bccc93272dcab2e93047993dfb
Lianhui Wang <lianhuiwang09@gmail.com>
2015-06-18 20:03:13 +0800

    add FindPeakElement && Repeated_DNA_Sequences

--- src/main/java/Find_Peak_Element/FindPeakElement.java
+++ src/main/java/Find_Peak_Element/FindPeakElement.java
@@ -1,9 +1,23 @@
 package Find_Peak_Element;
 
+/**
+ * https://leetcode.com/problems/find-peak-element/
+ * @author lianhui
+ *
+ */
 public class FindPeakElement {
+  
   public class Solution {
     public int findPeakElement(int[] nums) {
-        
+      int start = 0;
+      while (start < nums.length - 1) {
+        if (nums[start] > nums[start + 1]) {
+          break;
+        }
+        start ++;
+      }
+      return start;
     }
   }
+  
 }
--- src/main/java/Repeated_DNA_Sequences/Repeated_DNA_Sequences.java
+++ src/main/java/Repeated_DNA_Sequences/Repeated_DNA_Sequences.java
@@ -1,5 +1,36 @@
 package Repeated_DNA_Sequences;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/repeated-dna-sequences/
+ * @author lianhui
+ *
+ */
 public class Repeated_DNA_Sequences {
 
+  public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+      List<String> results = new ArrayList<String>();
+      Map<String,Boolean> maps = new HashMap<String,Boolean>();
+      for (int i = 0; i + 10 <= s.length(); i++){
+        String str = s.substring(i, i + 10);
+        if (maps.containsKey(str)) {
+          if (! maps.get(str)) {
+            results.add(str);
+            maps.put(str, true);
+          }
+        } else {
+          maps.put(str, false);
+        }
+      }
+      return results;
+    }
+  }
+  
 }
